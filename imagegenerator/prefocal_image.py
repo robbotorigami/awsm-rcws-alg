887f477d224d0b28c945e8e94e379634321b9012
Robert Belter <robbotorigami@gmail.com>
2017-11-30 17:12:01 -0700

    Commiting to save

---
+++ imagegenerator/prefocal_image.py
@@ -0,0 +1,24 @@
+import proper
+import matplotlib.pyplot as plt
+
+def prefocal_image(wavelength, gridsize, PASSVAL):
+    diam = PASSVAL['diam']
+    focal_length = PASSVAL['focal_length']
+    beam_ratio = PASSVAL['beam_ratio']
+    det_size = PASSVAL['det_size']
+
+    wfo = proper.prop_begin(diam*2, wavelength, gridsize, beam_ratio)
+
+    proper.prop_circular_aperture(wfo, diam)
+    proper.prop_define_entrance(wfo)
+    proper.prop_lens(wfo, focal_length)
+    proper.prop_circular_aperture(wfo, diam)
+    proper.prop_zernikes(wfo, [i+1 for i in range(len(PASSVAL['ZERN']))], PASSVAL['ZERN'])
+
+    proper.prop_propagate(wfo, focal_length - PASSVAL['DEFOCUS'])
+    #plt.imshow(proper.prop_get_amplitude(wfo))
+    #plt.show()
+
+    (wfo, sampling) = proper.prop_end(wfo)
+
+    return (wfo, sampling)
